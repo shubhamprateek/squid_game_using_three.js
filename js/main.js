14225d747ecdf7919f2893353af80f9cf04f9545
shubham.prateek <sprateek2338@gmail.com>
2022-05-24 10:36:37 +0530

    Added Remaining Commits

--- js/main.js
+++ js/main.js
@@ -9,21 +9,26 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+const light = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
+scene.add(light);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+//scene.add(cube);
 
 camera.position.z = 5;
 
-const loader = new GLTFLoader();
-
+const loader = new THREE.GLTFLoader();
+loader.load("../models/scene.gltf", function (gltf) {
+  scene.add(gltf.scene);
+  gltf.scene.scale.set(0.4, 0.4, 0.4);
+});
 function animate() {
   renderer.render(scene, camera);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  cube.rotation.z += 0.01;
+  //cube.rotation.x += 0.01;
+  //cube.rotation.y += 0.01;
+  //cube.rotation.z += 0.01;
   requestAnimationFrame(animate);
 }
 animate();
